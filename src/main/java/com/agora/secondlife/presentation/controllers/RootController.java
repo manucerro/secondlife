9a8a85d2627a678a04fecd780e3289ce6f393489
MANUEL CERRO TORRES <6003304@viewnext.com>
2025-06-02 20:16:17 +0200

    Posible último cambio

--- src/main/java/com/agora/secondlife/presentation/controllers/RootController.java
+++ src/main/java/com/agora/secondlife/presentation/controllers/RootController.java
@@ -1,9 +1,9 @@
 package com.agora.secondlife.presentation.controllers;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class RootController {
 
     @GetMapping("/")
