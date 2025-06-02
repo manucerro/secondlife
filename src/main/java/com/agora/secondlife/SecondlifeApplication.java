b47abf06e99948645f85ec8a7f9aa88702856a15
MANUEL CERRO TORRES <6003304@viewnext.com>
2025-06-02 13:32:50 +0200

    Prueba despliegue

--- src/main/java/com/agora/secondlife/SecondlifeApplication.java
+++ src/main/java/com/agora/secondlife/SecondlifeApplication.java
@@ -3,11 +3,24 @@ package com.agora.secondlife;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @SpringBootApplication
 public class SecondlifeApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SecondlifeApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(SecondlifeApplication.class);
 
+        Map<String, Object> props = new HashMap<>();
+        String port = System.getenv("PORT");
+        if (port != null) {
+            props.put("server.port", port);
+        } else {
+            props.put("server.port", "8080");
+        }
+        app.setDefaultProperties(props);
+
+        app.run(args);
+    }
 }
